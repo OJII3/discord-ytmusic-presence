6baa23e5e6dcf7c82586a1641e62d973fe7f31b3
ojii3 <84656786+OJII3@users.noreply.github.com>
2024-08-05 01:45:06 +0900

    あとはjsonをパースするだけ!

--- chrome_extension/src/content_script.ts
+++ chrome_extension/src/content_script.ts
@@ -1,21 +1,32 @@
-// port.postMessage({ message: "ping", body: "hello from browser extension" });
-setInterval(() => {
-	const query = "img.ytmusic-player-bar";
-	const thumbnailElement = document.querySelector(query);
-	if (!thumbnailElement) {
-		throw new Error(`Element not found with query: ${query}`);
+let intervalId: Timer | null = null;
+
+window.onload = () => {
+	// avoid multiple interval instances
+	if (intervalId !== null) {
+		clearInterval(intervalId);
 	}
-	const thumbnailUrl = thumbnailElement.getAttribute("src");
-	console.log(thumbnailUrl);
-	// send to local discord-sdk process
-	fetch("http://localhost:8477", {
-		method: "POST",
-		headers: {
-			"Content-Type": "application/json",
-		},
-		body: JSON.stringify({
-			message: "thumbnail",
-			body: thumbnailUrl,
-		}),
-	});
-}, 5000);
+
+	intervalId = setInterval(() => {
+		const query = "img.ytmusic-player-bar";
+		const thumbnailElement = document.querySelector(query);
+		if (!thumbnailElement) {
+			throw new Error(`Element not found with query: ${query}`);
+		}
+		const thumbnailUrl = thumbnailElement.getAttribute("src");
+
+		try {
+			fetch("http://localhost:8477", {
+				method: "POST",
+				headers: {
+					"Content-Type": "application/json",
+				},
+				body: JSON.stringify({
+					music_url: "thumbnail",
+					thumbnail_url: thumbnailUrl,
+				}),
+			});
+		} catch (e) {
+			console.warn("Failed to send thumbnail to discord-sdk process", e);
+		}
+	}, 5000);
+};
--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,61 @@
 mod playerctl;
-mod server;
 
-use std::slice::RChunks;
+use futures_util::future::join;
+use once_cell::sync::Lazy;
+use serde::Deserialize;
+use std::sync::Mutex;
+use std::thread;
 use std::time::Duration;
+
+use std::convert::Infallible;
+use std::net::SocketAddr;
+
+use http_body_util::{BodyExt, Full};
+use hyper::body::{Buf, Bytes};
+use hyper::server::conn::http1;
+use hyper::service::service_fn;
+use hyper::{body::Incoming as IncompingBody, Request, Response};
+use hyper_util::rt::TokioIo;
+use tokio::net::TcpListener;
 
 use anyhow::bail;
 use discord_sdk::activity::ActivityBuilder;
 use discord_sdk::activity::Assets;
 use discord_sdk::activity::Button;
-// use discord_presence::{Client, Event};
-// use std::thread::sleep;
 use discord_sdk::wheel::UserState;
 use discord_sdk::wheel::Wheel;
-use discord_sdk::AppId;
-use discord_sdk::Discord;
-use discord_sdk::DiscordApp;
+use discord_sdk::{AppId, Discord, DiscordApp};
+use tokio;
 
 use anyhow::Context as _;
 use discord_sdk::Subscriptions;
 use mpris::{PlayerFinder, ProgressTick};
-// use tokio::time::sleep;
 use tracing::{error, info};
 
 const APP_ID: AppId = 1267187960227565651;
+
+#[derive(Deserialize, Debug)]
+struct Music {
+    music_url: String,
+    thumbnail_url: String,
+}
+
+impl Clone for Music {
+    fn clone(&self) -> Self {
+        Music {
+            music_url: self.music_url.clone(),
+            thumbnail_url: self.thumbnail_url.clone(),
+        }
+    }
+}
+
+static CURRENT_MUSIC: Lazy<Mutex<Music>> = Lazy::new(|| {
+    let music = Mutex::new(Music {
+        music_url: "https://open.spotify.com/track/7lPN2DXiMsVn7XUKtOW1CS?si=8b1".to_string(),
+        thumbnail_url: "./media/ytmusic.png".to_string(),
+    });
+    music
+});
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
@@ -61,51 +94,99 @@ async fn main() -> anyhow::Result<()> {
         }
     });
 
-    tokio::task::spawn(async move { server::serve() });
+    async fn handle_request(
+        req: Request<IncompingBody>,
+    ) -> Result<Response<Full<Bytes>>, Infallible> {
+        let whole_body = req.collect().await.unwrap().aggregate();
+        // Decode as JSON...
+        let mut data: serde_json::Value = serde_json::from_reader(whole_body.reader()).unwrap();
+        // Change the JSON...
+        match serde_json::from_value::<Music>(data.clone()) {
+            Ok(music) => {
+                println!("Music: {:?}", music);
+                CURRENT_MUSIC.lock().unwrap().thumbnail_url = music.thumbnail_url;
+            }
+            Err(_) => {
+                let current_music = CURRENT_MUSIC.lock().unwrap();
+                data["music_url"] = serde_json::Value::String(current_music.music_url.clone());
+                data["thumbnail_url"] =
+                    serde_json::Value::String(current_music.thumbnail_url.clone());
+            }
+        }
+        // And respond with the new JSON.
+        Ok(Response::new(Full::new(Bytes::from("Hello World!"))))
+    }
+
+    let addr = SocketAddr::from(([127, 0, 0, 1], 8477));
+    let server = async move {
+        let listener = TcpListener::bind(addr).await.unwrap();
+        loop {
+            let (tcp, _) = listener.accept().await.unwrap();
+
+            // Use an adapter to access something implementing `tokio::io` traits as if they implement
+            // `hyper::rt` IO traits.
+            let io = TokioIo::new(tcp);
+
+            // Spawn a tokio task to serve multiple connections concurrently
+            tokio::task::spawn(async move {
+                // Finally, we bind the incoming connection to our `hello` service
+                if let Err(err) = http1::Builder::new()
+                    // `service_fn` converts our function in a `Service`
+                    .serve_connection(io, service_fn(handle_request))
+                    .await
+                {
+                    eprintln!("Error serving connection: {:?}", err);
+                }
+            });
+        }
+    };
+    info!("Server started at http://{}", addr);
 
     // Create Playerctl instance
-    loop {
-        let Ok(_player) = PlayerFinder::new().unwrap().find_active() else {
-            continue;
-        };
-        let player = PlayerFinder::new().unwrap().find_active().unwrap();
-        let identity = player.identity();
-        println!("Found player: {}", identity);
-        let mut progress_tracker = player.track_progress(1000).unwrap();
-        let ProgressTick { progress, .. } = progress_tracker.tick();
 
-        let Ok(_metadata) = player.get_metadata() else {
-            continue;
-        };
-        playerctl::print_playback_status(progress);
-        playerctl::print_shuffle_status(progress);
-        playerctl::print_loop_status(progress);
-        let artist = playerctl::get_artist(progress.metadata());
-        let title = playerctl::get_title(progress.metadata());
-        let time_elapsed = playerctl::get_time(Some(progress.position()));
-        let time_total = playerctl::get_time(progress.length());
-        let thumbnail = playerctl::get_thumbnail(progress.metadata());
-        println!(
-            "Artist: {}\nTitle: {}\nTime Elapsed: {}\nTime Total: {}\nThumbnail: {}",
-            artist, title, time_elapsed, time_total, thumbnail
-        );
+    let discord_process = async move {
+        loop {
+            let Ok(_player) = PlayerFinder::new().unwrap().find_active() else {
+                continue;
+            };
+            let player = PlayerFinder::new().unwrap().find_active().unwrap();
+            let identity = player.identity();
+            println!("Found player: {}", identity);
+            let mut progress_tracker = player.track_progress(1000).unwrap();
+            let ProgressTick { progress, .. } = progress_tracker.tick();
 
-        let rich_presence = ActivityBuilder::default()
-            .assets(
-                Assets::default().large("https://lh3.googleusercontent.com/1MvteP7Nv0sytWfj369xg7hH-xmaC8C3hhEJcxJxZPY9pArXicvyA0hKp7CpeRS0qwiFGbQ1dshfXic=w60-h60-l90-rj", Some("YT Music")),
-            )
-            .state(format!("by {} - {} / {}", artist, time_elapsed, time_total))
-            .details(title)
-            .button(Button {
-                label: "Open".to_string(),
-                url: "https://open.spotify.com".to_string(),
-            });
+            let Ok(_metadata) = player.get_metadata() else {
+                continue;
+            };
+            playerctl::print_playback_status(progress);
+            playerctl::print_shuffle_status(progress);
+            playerctl::print_loop_status(progress);
+            let artist = playerctl::get_artist(progress.metadata());
+            let title = playerctl::get_title(progress.metadata());
+            let time_elapsed = playerctl::get_time(Some(progress.position()));
+            let time_total = playerctl::get_time(progress.length());
+            let thumbnail_url = CURRENT_MUSIC.lock().unwrap().clone().thumbnail_url;
 
-        info!(
-            "updated activity: {:?}",
-            discord.update_activity(rich_presence).await?
-        );
+            let rich_presence = ActivityBuilder::default()
+                .assets(Assets::default().large(thumbnail_url, Some("YT Music")))
+                .state(format!("by {} - {} / {}", artist, time_elapsed, time_total))
+                .details(title)
+                .button(Button {
+                    label: "Open".to_string(),
+                    url: "https://open.spotify.com".to_string(),
+                });
 
-        // sleep(Duration::from_secs(1)).await;
-    }
+            info!(
+                "updated activity: {:?}",
+                discord.update_activity(rich_presence).await
+            );
+
+            // From others, presence is updated every 5 seconds, so no need to update it every second
+            thread::sleep(Duration::from_secs(1));
+        }
+    };
+
+    let _result = join(server, discord_process).await;
+
+    Ok(())
 }
